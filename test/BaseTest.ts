3d7bb6c4911060e36d2073c9dd6ebd6820683adc
logicalpath <eddie@logicalpath.com>
2020-06-27 21:00:19 -0700

    mention the-internet and commitvarious changes that had been in limbo

--- test/BaseTest.ts
+++ test/BaseTest.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
-'use strict';
-import * as fs from 'fs';
-import * as webDriver from 'selenium-webdriver';
-import DriverFactory from '../lib/DriverFactory';
-import { Driver } from 'selenium-webdriver/chrome';
+"use strict";
+import * as fs from "fs";
+import * as webDriver from "selenium-webdriver";
+import DriverFactory from "../lib/DriverFactory";
+import { Driver } from "selenium-webdriver/chrome";
 
 let driver: webDriver.WebDriver;
 let driverFactory: { driver: webDriver.WebDriver; quit: () => void };
@@ -12,34 +12,32 @@ let screenshotName;
 let artifactsPath;
 
 afterEach(function () {
-    const xyz = 'what';
-    console.log(xyz);
-    const testStatus = this.currentTest?.state;
-    const testName = this.currentTest?.title;
-    const prettyName = testName?.replace(/ /g, '');
+  const testStatus = this.currentTest?.state;
+  const testName = this.currentTest?.title;
+  const prettyName = testName?.replace(/ /g, "");
 
-    if (testStatus === 'failed') {
-        driverFactory.driver.takeScreenshot().then((data) => {
-            screenshotName = 'Failed-' + prettyName;
-            artifactsPath = `artifacts/${screenshotName}.png`;
-            fs.writeFileSync(artifactsPath, data, 'base64');
-        });
-    }
+  if (testStatus === "failed") {
+    driverFactory.driver.takeScreenshot().then((data) => {
+      screenshotName = "Failed-" + prettyName;
+      artifactsPath = `artifacts/${screenshotName}.png`;
+      fs.writeFileSync(artifactsPath, data, "base64");
+    });
+  }
 });
 
 function getDriver() {
-    driverFactory = new DriverFactory();
-    driver = driverFactory.driver;
-    return driver;
+  driverFactory = new DriverFactory();
+  driver = driverFactory.driver;
+  return driver;
 }
 
 function quitDriver() {
-    driverFactory.quit();
-    console.log('From BaseTest: quitDriver: after call to quit');
+  driverFactory.quit();
+  console.log("From BaseTest: quitDriver: after call to quit");
 }
 
 function getScreenShotFiles() {
-    return screenShotFiles;
+  return screenShotFiles;
 }
 
 export { getDriver, getScreenShotFiles, quitDriver };
